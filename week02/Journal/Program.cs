a70a6e490234776085715f25921a519bedc004e9
UdoeyenOkon <uokon2@byupathway.edu>
2026-01-19 13:40:45 +0100

    Journal program design framework

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Journal Project.");
+
+        Journal myJournal = new Journal();
+
+
+        Entry anEntry = new Entry();
+        anEntry.Display();
+
+
+        PromptGenerator prompt = new PromptGenerator();
+        prompt.GetRandomPrompt();
+
+
     }
 }
---
+++ week02/Journal/PromtGenerator.cs
@@ -0,0 +1,10 @@
+public class PromptGenerator
+{
+    public List<string> _promts;
+
+    public string GetRandomPrompt()
+    {
+        return "";
+    }
+
+}
