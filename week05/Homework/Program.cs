9b20ce01bdb47a217700b6621e225db9801fe7d6
UdoeyenOkon <uokon2@byupathway.edu>
2026-02-04 16:52:29 +0100

    Inheritance assignment completed

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,22 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+
+        Assignment assignment1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assignment1.GetSummary());
+
+        Console.WriteLine();
+
+        MathAssignment mathAssignment1 = new MathAssignment("Roberto Rodriquez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment1.GetSummary());
+        Console.WriteLine(mathAssignment1.GetHomeworkList());
+
+        Console.WriteLine();
+
+        WritingAssignment writingAssignment1 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment1.GetSummary());
+        Console.WriteLine(writingAssignment1.GetWritingInformation());
+
     }
+
 }
